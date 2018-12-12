aa5bcea0608fa4d82abcdd67c47e3ec84fc80ccd
Irina Usoltceva <irina.710@mail.ru>
2018-12-12 21:45:39 +0300

    созднаеие

---
+++ scripts/script.js
@@ -0,0 +1,41 @@
+//фиксированное меню
+var h_hght = 100; // высота шапки
+var h_mrg = 0;    // отступ когда шапка уже не видна
+                 
+$(function(){
+ 
+    var elem = $('#menu');
+    var top = $(this).scrollTop();
+     
+    if(top > h_hght){
+        elem.css('top', h_mrg);
+    }           
+     
+    $(window).scroll(function(){
+        top = $(this).scrollTop();
+         
+        if (top+h_mrg < h_hght) {
+            elem.css('top', (h_hght-top));
+        } else {
+            elem.css('top', h_mrg);
+        }
+    });
+ 
+});
+
+//плавный переход
+$(document).ready(function(){
+	$("#menu").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор блока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+});
